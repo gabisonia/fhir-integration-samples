e3477d713fee5d8eac48eca86b636e6faa07ed45
gabisonia <irakli.gabisonia94@gmail.com>
2023-09-10 12:10:24 +0400

    Add test practitioner Id

--- Hapi.Samples/Program.cs
+++ Hapi.Samples/Program.cs
@@ -2,13 +2,14 @@
 
 Console.WriteLine("Hello, Hapi Server!");
 
-// var organizationManager = new OrganizationManager();
-// await organizationManager.GetAllOrganizationsAsync();
-//
-//
-// var practitionerManager = new PractitionerManagement();
-// // test organizationId 37751
-// await practitionerManager.GetAllPractitionersAsync();
+var organizationManager = new OrganizationManager();
+await organizationManager.GetAllOrganizationsAsync();
+
+
+var practitionerManager = new PractitionerManagement();
+// test organizationId 37751
+await practitionerManager.GetAllPractitionersAsync();
 
 var scheduleManager = new ScheduleManager();
+// test practitionerId 37751
 await scheduleManager.FetchSchedulesAndTimeSlotsForPractitionerAsync("1566020");
